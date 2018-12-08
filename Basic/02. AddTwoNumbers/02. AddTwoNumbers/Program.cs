9391bbb7e9dc1b72cfd55867c21519c5ebdc92fc
Mihail Hadzhiev <37671971+mishondrakis@users.noreply.github.com>
2018-12-08 19:24:54 +0200

    Basic C#

---
+++ Basic/02. AddTwoNumbers/02. AddTwoNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _02._AddTwoNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var sum = a + b;
+            Console.WriteLine($"{a} + {b} = {sum}");
+        }
+    }
+}
